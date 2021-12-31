9e751e816e0c9a22bc27439f4cc531f9efdedd78
aditideepak <aditi.dpk17@gmail.com>
2021-12-31 12:54:36 +0530

    [Aditi] - Test for user input

--- todo-react-tdd/cypress/integration/input_form.spec.js
+++ todo-react-tdd/cypress/integration/input_form.spec.js
@@ -1,8 +1,21 @@
 describe('Input form', () => {
-    it('focuses input on load', () => {
-        cy.visit("http://localhost:3000")
 
+    beforeEach(() => {
+        cy.visit("http://localhost:3000")
+    })
+
+    /*Tests if the focused element belongs to the defined class*/
+    it('focuses input on load', () => {
         cy.focused()
         .should('have.class', 'new-todo')
+    })
+
+    /*Ensures that text input has exactly what the user types*/
+    it('accepts input', () => {
+        const typedText = 'Complete Prereads'
+
+        cy.get('.new-todo')
+          .type(typedText)
+          .should('have.value', typedText)
     })
 })
